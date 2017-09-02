10ad19ce788acf81f7a1345dc07cfc800ee460af
palyon <patrick@Patricks-MacBook-Pro.local>
2017-09-02 13:07:46 -0400

    Completed Assignment Push

---
+++ level3.js
@@ -0,0 +1,37 @@
+function lvl3exercise1() {
+	// Create a "hello" and a "world", return the concatenation of the two
+  var one = "hello";
+	var two = "world";
+	return one + two;
+}
+
+function lvl3exercise2() {
+	// Create a "hello" and a 12, return the concatenation of the two
+	var one = "hello";
+	var two = 12;
+	return one + two;
+
+}
+
+function lvl3exercise3() {
+	// Create a variable that equals 12 and convert it to a string with concatenation. Return it.
+	var one = 12;
+	var two = "";
+	return one + two;
+
+}
+
+function lvl3exercise4() {
+	// Create a "hello world!" string. Return the length of the string
+	var one = "hello world!";
+	var two = one.length;
+	return two;
+
+}
+
+function lvl3exercise5() {
+	// Create a "hello world!" string. Return the index of the word "world".
+  var one = "hello world!";
+	var two = one.indexOf("world");
+	return two;
+}
---
+++ level4.js
@@ -0,0 +1,41 @@
+function lvl4exercise1() {
+	// Return the boolean value "true"
+	var boolean = true;
+	return boolean;
+
+}
+
+function lvl4exercise2() {
+	// Return the boolean value "false"
+	var boolean = false;
+	return boolean;
+
+}
+
+function lvl4exercise3(bool) {
+	// Return the opposite of the input boolean value
+	var bool = !bool;
+	return bool;
+
+}
+
+function lvl4exercise4(bool1, bool2) {
+	// Return the logical "and" of the input boolean values
+	var one = bool1 && bool2;
+	return one;
+
+}
+
+function lvl4exercise5(bool1, bool2) {
+	// Return the logical "or" of the input boolean values
+	var one = bool1 || bool2;
+	return one;
+
+}
+
+function lvl4exercise6(bool1, bool2) {
+	// Return the logical "equivalence" of the input boolean values
+	var one = bool1 == bool2;
+	return one;
+
+}
